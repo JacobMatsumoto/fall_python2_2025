e32b211b14ddcd259491adf3023fe0fc24471536
Jacob <jmatsumoto@students.mchenry.edu>
2025-10-19 23:55:48 -0500

    gui 1.6 done

--- assignments/gui16.py
+++ assignments/gui16.py
@@ -1,5 +1,8 @@
 
 import tkinter as tk
+from tkinter import messagebox
+from datetime import datetime
+
 
 # Initialize the main window
 root = tk.Tk()
@@ -25,23 +28,52 @@ def change_background():
         root.configure(bg="red")
         BACKGROUNDCOLORCYCLE = 1
 
-poem = ["Roses are red", "Violets are blue", "Sugar is sweet", "and so are you"] 
+
+poem = ["Roses are red", "Violets are blue",
+        "Sugar is sweet", "and so are you"]
 # I looked it up and you can do it like this https://labex.io/tutorials/python-how-to-retain-state-between-function-calls-420193
 
-class counter:
+
+class Counter:
     def __init__(self):
         self.count = 0
 
     def increment(self):
         self.count += 1
+        if self.count > 3:
+            self.count = 0
         return self.count
 
 
+poem_counter = Counter()
 
+
+poems_label = tk.Label(root, text=poem[poem_counter.count], background="brown")
+poems_label.pack(pady=10)
+
+
+def update_poem_line():
+    poem_counter.increment()
+    current_index = poem_counter.count
+
+    poems_label.config(text=poem[current_index])
+
+
+poem_change = tk.Button(root, text="Next line", command=update_poem_line)
+poem_change.pack(pady=10)
 window_button = tk.Button(
     root, text="Change background", command=change_background)
 window_button.pack(pady=10)
 
 
+# https://www.geeksforgeeks.org/python/get-current-date-and-time-using-python/
+def show_date_time():
+    now = datetime.now()
+    current_time = f"Year:{now.year} Month:{now.month} Day:{now.day} Hour:{now.hour} Minute:{now.minute}"
+    messagebox.showinfo("Current Time", current_time)
+
+
+date_time_button = tk.Button(root, text="Show time", command=show_date_time)
+date_time_button.pack(pady=10)
 # Start the Tkinter event loop
 root.mainloop()
